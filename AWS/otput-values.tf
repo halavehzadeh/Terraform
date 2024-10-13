eb06098d119b974a6d4aa7520db8b8b2fe007b53
halavehzadeh <halavehzadeh@outlook.com>
2024-10-13 11:10:51 -0400

    add output-values

---
+++ AWS/otput-values.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1" 
+}
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+#show ip address in screen 
+output "public-ip" {
+    value = aws_eip.lb.public_ip
+}
