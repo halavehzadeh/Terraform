7217f736348ffbadbb10fbf962622b8d678e7e13
halavehzadeh <halavehzadeh@outlook.com>
2024-10-13 17:44:26 -0400

    Create variables

---
+++ AWS/variables/variable.tf
@@ -0,0 +1,17 @@
+variable "vpn_ip" {
+	default = "101.20.30.50/32"
+	description = "This is a VPN Server Created in AWS"
+}
+
+variable "app_port" {
+	default = "8080"
+}
+
+variable "ssh_port" {
+	default = "22"
+}
+
+
+variable "ftp_port" {
+	default = "21"
+}
