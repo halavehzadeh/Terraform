f2450de0a6bdbdb52ff3aef1e99db3d2fbb0fd45
halavehzadeh <halavehzadeh@outlook.com>
2024-10-14 12:42:18 -0400

    add enviroment variables

---
+++ AWS/enviroment varibales/Variables.tf
@@ -0,0 +1,8 @@
+#These varibale should be defined in system
+
+variable "instance_type" {}
+variable "ami" {}
+variable "vpn_ip" {}
+variable "app_port" {}
+variable "ssh_port" {}
+variable "ftp_port" {}
